d69690e1794ca8fa879f6ad33be870c9df5fc993
Leonardo Rodrigues <22543116-leux08@users.noreply.gitlab.com>
2024-12-19 00:39:40 -0300

    close theme music pop up

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+var closethememusicpopup = document.getElementById("closethememusicbutton");
+var thememusicpopup = document.getElementById("thememusic")
+
+closethememusicpopup.addEventListener('click', function() {
+    thememusicpopup.style.display = "none"
+  });
