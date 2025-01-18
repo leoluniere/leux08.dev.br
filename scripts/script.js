162842616326292c37844b482e115dd84a4b3eaa
Leonardo Rodrigues <22543116-leux08@users.noreply.gitlab.com>
2025-01-18 17:48:16 -0300

    a

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,3 @@
-importScripts("https://cdn.pushalert.co/sw-78571.js");
-
 var closethememusicpopup = document.getElementById("closethememusicbutton");
 var thememusicpopup = document.getElementById("thememusic")
 
